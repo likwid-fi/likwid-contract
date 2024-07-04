38a4f9c3b5c3900694b70e68f7f6869f82425a9d
likwid <op@likwid.meme>
2024-07-04 16:30:40 +0800

    modify

--- task/devSupply.ts
+++ task/devSupply.ts
@@ -5,7 +5,7 @@ export default async (args: { validator: string }, hre: HardhatRuntimeEnvironmen
   const { ethers, getNamedAccounts, deployments } = hre;
   const [deployer] = await ethers.getSigners();
   const chain = hre.network.name;
-  const slaveChains = ['Base', 'Linea', 'Scroll', 'Optimism', 'Arbitrum', 'zkEVM', 'Blast', 'Bob'];
+  const slaveChains = ['Taiko', 'Base', 'Linea', 'Scroll', 'Optimism', 'Arbitrum', 'zkEVM', 'Blast', 'Bob'];
   if (slaveChains.includes(chain)) {
     const slaveAddr = getDeploymentAddresses(chain)['TokenSlave'];
     const slave = await ethers.getContractAt('TokenSlave', slaveAddr);
@@ -16,7 +16,7 @@ export default async (args: { validator: string }, hre: HardhatRuntimeEnvironmen
   if (chain == 'Vizing') {
     const masterAddr = getDeploymentAddresses(chain)['TokenMaster'];
     const master = await ethers.getContractAt('TokenMaster', masterAddr);
-    console.log(`Chain(vizing_testnet)`);
+    console.log(`Chain(${chain})`);
     console.log(`Contract(${master.address})`);
     console.log(`totalSupply:\n${await master.totalSupply()}`);
   }
